268d2d289b7efc1cc7c4959d1ddaf96753674a14
Alejandra <146900779+AlejandraMedranda0906@users.noreply.github.com>
2025-02-18 21:48:25 -0500

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
-import Home from "./pages/Inicio";
+import Inicio from "./pages/Inicio";
 import Acerca from "./pages/Acerca";
-import Presupuesto from "./pages/Presupuesto";
-import Metodologia from "./pages/Metodologia";
-import Galeria from "./pages/Galeria";
 import Resultados from "./pages/Resultados";
 import Conclusion from "./pages/Conclusion";
 
@@ -15,7 +12,7 @@ const App: React.FC = () => {
       <Header />
       <Navigation />
       <Routes>
-        <Route path="/" element={<Inicio />} />
+        <Route path="/Inicio" element={<Inicio />} />
         <Route path="/acerca" element={<Acerca />} />
         <Route path="/resultados" element={<Resultados />} />
         <Route path="/conclusion" element={<Conclusion />} />
---
+++ src/pages/Inicio.tsx
@@ -0,0 +1,11 @@
+const Inicio: React.FC = () => {
+    return (
+      <div>
+        <h2>kjvkghb</h2>
+        <p>Resumen final del proyecto y aprendizajes obtenidos...</p>
+      </div>
+    );
+  };
+  
+  export default Inicio;
+  
