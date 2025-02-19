7c811e451be4ea8c20a086e984cfe22bb3869756
Alejandra <146900779+AlejandraMedranda0906@users.noreply.github.com>
2025-02-19 00:14:17 -0500

    acerca

--- src/pages/Acerca.tsx
+++ src/pages/Acerca.tsx
@@ -1,29 +1,32 @@
 import "../styles/Acerca.css";
 
-
 const Acerca = () => {
   return (
     <section className="acerca-container">
-      <div className="seccion izquierda">
-        <h2>Metodologia</h2>
-        <p>Este proyecto busca...</p>
-      </div>
-
-      <div className="seccion centro">
-        <h2>Presupuesto</h2>
-        <p>Aquí se describe la metodología utilizada...</p>
-      </div>
-
       <div className="seccion derecha">
-        <h2>Diseño</h2>
-        <div className="imagenes">
-          <img src="/images/diseño1.png" alt="Diseño 1" />
+        <h2>Metodología</h2>
+        <div className="metodologia-contenido">
+          <p>Este proyecto busca...</p>
           <img src="/images/diseño2.png" alt="Diseño 2" />
         </div>
       </div>
 
-      <div className="seccion presupuesto">
-        <h2>Presupuesto y Prototipo</h2>
+      <div className="seccion centro">
+        <h2>Presupuesto</h2>
+        <img src="/images/diseño2.png" alt="Diseño 2" />
+        <p>Aquí se describe la metodología utilizada...</p>
+      </div>
+
+      <div className="seccion derecha">
+        <h2>Presupuesto</h2>
+        <div className="imagenes">
+          <img src="/images/diseño2.png" alt="Diseño 2" />
+          <p>Aquí se describe la metodología utilizada...</p>
+        </div>
+      </div>
+
+      <div className="seccion prototipo">
+        <h2>Prototipo</h2>
         <div className="media-container">
           <video controls>
             <source src="/videos/prototipo.mp4" type="video/mp4" />
