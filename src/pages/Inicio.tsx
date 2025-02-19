37e126e7d3792f0f7673c1286e98f3115a1668ab
Alejandra <146900779+AlejandraMedranda0906@users.noreply.github.com>
2025-02-19 07:42:30 -0500

    .

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -15,16 +15,18 @@ const Inicio: React.FC = () => {
       </section>
 
       {/* SECCIÓN CONTENIDO */}
-      <section className="contenido">
+      <section className="contenido justificacion-introduccion">
+
         <h2>Justificación</h2>
-        <p>Aquí va más información sobre el proyecto...</p>
-        <p>Secciones adicionales...</p>
+        <p>La implementación de dinamos en bicicletas estáticas permite generar energía limpia mientras se realiza ejercicio, reduciendo la dependencia de fuentes no renovables. </p>
+        
       </section>
 
-      <section className="contenido">
+      
+<section className="contenido justificacion-introduccion">
         <h2>Introducción</h2>
         <p>El  proyecto busca implementar un sistema que produzca energía limpia en los gimnasios a través de un generador colocado en bicicletas estáticas. Esta iniciativa utiliza la energía del movimiento del pedaleo para transformarla en electricidad, ayudando a la sostenibilidad y fomentando la actividad física.</p>
-        <p>Secciones adicionales...</p>
+        
       </section>
 
       {/* SECCIÓN OBJETIVOS */}
